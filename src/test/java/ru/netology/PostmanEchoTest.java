bb35b2b186b548ea457a916a913140271fb3b038
Artpogorelov <110556248+Artpogorelov@users.noreply.github.com>
2022-12-26 21:25:45 +0300

    Update PostmanEchoTest.java

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -6,16 +6,16 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
-    @Test
-    public void shouldPostRawTextTest() {
+       @Test
+    public void shouldPostToPostmanEcho() {
         given()
                 .baseUri("https://postman-echo.com")
-                .contentType("text/plain; charset=UTF-8")
-                .body("Это что, API автотест?")
-                .when()
+                .body("some data")
+        .when()
                 .post("/post")
-                .then()
+        .then()
                 .statusCode(200)
-                .body("data", equalTo("Это что, API автотест?"));
+                .body("data", equalTo("some data"))
+        ;
     }
 }
